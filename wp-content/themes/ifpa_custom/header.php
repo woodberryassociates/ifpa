83dfeb35283c438ccd85a0e6d28c1dac7abee1e2
Avi Nerenberg <devavmeister@gmail.com>
2017-10-11 00:15:06 -0400

    first modifications to nav

--- wp-content/themes/ifpa_custom/header.php
+++ wp-content/themes/ifpa_custom/header.php
@@ -35,14 +35,15 @@
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
 							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo" class="logo-img">
 						</a>
 					</div>
 					<!-- /logo -->
 
 					<!-- nav -->
 					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
+						<!--?php html5blank_nav(); ?-->
+						POLICY PAPERS / INFOGRAPHICS / BLOG / REPORT CARDS / STUDIES / ANALYSIS / SUMMITS
 					</nav>
 					<!-- /nav -->
 
