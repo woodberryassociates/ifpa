4a7ea05112af733787f8dc8b2c6f132cbfaf690c
Avi Nerenberg <devavmeister@gmail.com>
2017-11-17 21:14:59 -0500

    updated homepage

--- wp-content/themes/ifpa_custom/home-page.php
+++ wp-content/themes/ifpa_custom/home-page.php
@@ -8,52 +8,45 @@
 
 		<!-- article -->
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
 			<?php the_content(); ?>
-
 			<br class="clear">
-
-			<?php edit_post_link(); ?>
-
 		</article>
 		<!-- /article -->
 
 	</section>
 
 	<section class="home-block">
-		<?php $catquery = new WP_Query( 'cat=policy-papers&posts_per_page=1' ); ?>
-
+		<section class="home-block-article">
+			<?php $catquery = new WP_Query( 'cat=5&posts_per_page=1' ); ?>
 			<?php while($catquery->have_posts()) : $catquery->the_post(); ?>
-
 				<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
+				<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_post_thumbnail(); ?></a>
 			<?php endwhile;
 			wp_reset_postdata();
 			?>
-	</section>
+		</section>
 
-	<section class="home-block">
-		<?php $catquery = new WP_Query( 'cat=policy-papers&posts_per_page=1' ); ?>
-
+		<section class="home-block-article">
+			<?php $catquery = new WP_Query( 'cat=6&posts_per_page=1' ); ?>
 			<?php while($catquery->have_posts()) : $catquery->the_post(); ?>
-
 				<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
+				<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_post_thumbnail(); ?></a>
 			<?php endwhile;
 			wp_reset_postdata();
 			?>
-	</section>
+		</section>
 
-	<section class="home-block">
-		<?php $catquery = new WP_Query( 'cat=policy-papers&posts_per_page=1' ); ?>
-
+		<section class="home-block-article">
+			<?php $catquery = new WP_Query( 'cat=7&posts_per_page=1' ); ?>
 			<?php while($catquery->have_posts()) : $catquery->the_post(); ?>
-
 				<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
+				<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_post_thumbnail(); ?></a>
 			<?php endwhile;
 			wp_reset_postdata();
 			?>
+		</section>
 	</section>
 </main>
 
 <?php get_sidebar(); ?>
-
 <?php get_footer(); ?>
