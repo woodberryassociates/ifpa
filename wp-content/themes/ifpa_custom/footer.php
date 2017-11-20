c0729b6e4bd609fe23d9cefde6c1ca8b11d27833
Avi Nerenberg <devavmeister@gmail.com>
2017-11-20 00:11:18 -0500

    attempting to remedy some discrepancies with staging site

--- wp-content/themes/ifpa_custom/footer.php
+++ wp-content/themes/ifpa_custom/footer.php
@@ -1,3 +1,4 @@
+			<?php echo "</a>"; //for some reason this is necessary on the remote site ?>
 			<!-- footer -->
 			<footer class="footer" role="contentinfo">
 
@@ -18,13 +19,12 @@
 
 		<!-- analytics -->
 		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
+			(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+				(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+				l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+			ga('send', 'pageview');
 		</script>
-
 	</body>
 </html>
