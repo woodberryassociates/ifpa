2b0fe2cb9d3258ce849724cb3f64bdd951bf1100
Avi Nerenberg <devavmeister@gmail.com>
2017-11-30 00:00:14 -0500

    finished single.php

--- wp-content/themes/ifpa_custom/header.php
+++ wp-content/themes/ifpa_custom/header.php
@@ -3,7 +3,7 @@
 <head>
 	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 	<meta charset="<?php bloginfo('charset'); ?>">
-	<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+	<title>Institute for Patient Access</title>
 
 	<link href="//www.google-analytics.com" rel="dns-prefetch">
 	<link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
--- wp-content/themes/ifpa_custom/single.php
+++ wp-content/themes/ifpa_custom/single.php
@@ -4,7 +4,6 @@
 	<!-- section -->
 	<section class="single">
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
 		<!-- article -->
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
@@ -17,30 +16,23 @@
 			<!-- /post thumbnail -->
 
 			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
+			<article id="article-overlay">
+				<article id="content">
+					<h1>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><center><?php the_title(); ?></center></a>
+					</h1>
+					<!-- /post title -->
+					&nbsp;
+					<?php the_content(); ?>
+					&nbsp;
+					<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); ?>
+					&nbsp;
+					<p style="text-align: right;"><?php _e( 'Categorized in: ', 'html5blank' ); the_category(', '); ?></p>
+					<p style="text-align: right;"><?php edit_post_link(); ?></p>
 
-			<!-- post details -->
-			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-			<!--span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<--span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-			<!-- /post details -->
-
-			<?php the_content(); // Dynamic Content ?>
-
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
-
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
-
-			<!--p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p--
-			>
-
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
-
-			<!--?php comments_template(); ?-->
-
+					<?php get_footer(); ?>
+				</article>
+			</article>
 		</article>
 		<!-- /article -->
 
@@ -49,7 +41,7 @@
 	<?php else: ?>
 
 		<!-- article -->
-		<article>
+		<article id="content">
 
 			<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
 
@@ -60,6 +52,7 @@
 
 	</section>
 	<!-- /section -->
+
 	</main>
 
 <?php get_footer(); ?>
