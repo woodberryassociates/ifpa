2be03c0cb09e80540f23022854dfb7a2e5795609
Avi Nerenberg <devavmeister@gmail.com>
2017-12-07 23:02:17 -0500

    formatting loop for with or w/o thumbnail

--- wp-content/themes/ifpa_custom/loop.php
+++ wp-content/themes/ifpa_custom/loop.php
@@ -6,7 +6,7 @@
 
 		<!-- article -->
 		<article style="padding-left: 15%; display: block; height: 220px;" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			
+
 			<?php if (has_post_thumbnail()): // Check if thumbnail exists ?>
 				<!-- post thumbnail -->
 				<a style="float: left; height: 200px; width: 300px; text-align: center;" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
@@ -18,19 +18,25 @@
 					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
 				</h2>
 				<!-- /post title -->
+
+				<?php echo "<p style='display: flex; padding: 5px 10px;'"; // dumb hack to correctly style the excerpt ?>
+				<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+				<?php echo "</p><a style='display: flex; padding: 0px 10px;'"; ?>
+				<?php edit_post_link(); ?>
+				<?php echo "</a>"; ?>
 			<?php else: //if no thumbnail ?>
 				<!-- post title -->
 				<h2 style="display: inline; padding: 10px 10px 10px 310px;">
 					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
 				</h2>
 				<!-- /post title -->
-			<?php endif; ?>
 
-			<?php echo "<p style='display: flex; padding: 5px 10px;'"; // dumb hack to correctly style the excerpt ?>
-			<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
-			<?php echo "</p><a style='display: flex; padding: 0px 10px;'"; ?>
-			<?php edit_post_link(); ?>
-			<?php echo "</a>"; ?>
+				<?php echo "<p style='display: flex; padding: 5px 10px;'"; // dumb hack to correctly style the excerpt ?>
+				<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+				<?php echo "</p><a style='display: flex; padding: 0px 10px;'"; ?>
+				<?php edit_post_link(); ?>
+				<?php echo "</a>"; ?>
+			<?php endif; ?>
 		</article>
 		<!-- /article -->
 <?php endwhile; ?>
