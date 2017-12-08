9d9ef1c78b14eacdb4479ce5459e3020a3d2ba9c
Avi Nerenberg <devavmeister@gmail.com>
2017-12-07 23:00:41 -0500

    formatting loop for with or w/o thumbnail

--- wp-content/themes/ifpa_custom/loop.php
+++ wp-content/themes/ifpa_custom/loop.php
@@ -6,20 +6,25 @@
 
 		<!-- article -->
 		<article style="padding-left: 15%; display: block; height: 220px;" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+			
+			<?php if (has_post_thumbnail()): // Check if thumbnail exists ?>
+				<!-- post thumbnail -->
 				<a style="float: left; height: 200px; width: 300px; text-align: center;" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 					<?php the_post_thumbnail(array(300,200)); // Declare pixel size you need inside the array ?>
 				</a>
+				<!-- /post thumbnail -->
+				<!-- post title -->
+				<h2 style="display: inline; padding: 10px;">
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+				</h2>
+				<!-- /post title -->
+			<?php else: //if no thumbnail ?>
+				<!-- post title -->
+				<h2 style="display: inline; padding: 10px 10px 10px 310px;">
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+				</h2>
+				<!-- /post title -->
 			<?php endif; ?>
-			<!-- /post thumbnail -->
-
-			<!-- post title -->
-			<h2 style="display: inline; padding: 10px;">
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h2>
-			<!-- /post title -->
 
 			<?php echo "<p style='display: flex; padding: 5px 10px;'"; // dumb hack to correctly style the excerpt ?>
 			<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
