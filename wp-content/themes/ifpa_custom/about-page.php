382a4c20a1ed7d9508c767ba7db238c7ff831f03
Avi Nerenberg <devavmeister@gmail.com>
2017-12-12 01:35:33 -0500

    added to 'about us' page

--- wp-content/themes/ifpa_custom/about-page.php
+++ wp-content/themes/ifpa_custom/about-page.php
@@ -3,12 +3,46 @@
 <?php get_header(); ?>
 
 <main role="main">
-	<section>
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/profiles/David-Charles.png">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/profiles/Howard-Hoffberg.png">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/profiles/Jack-Shim.png">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/profiles/Robert-Yapundich.png">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/profiles/Robin-K.-Dore.png">
-	</section>
+	<center>
+		<section style="width: 50%;">
+			<img style="max-height: 200px; float: left; padding-right: 10px" src="<?php echo get_stylesheet_directory_uri(); ?>/img/profiles/David-Charles.png">
+			<p style="min-height: 200px; text-align: justify;">
+				<strong>David Charles, M.D.<br>
+				Nashville, Tennessee</strong><br><br><br><br><br><br><br><!--Look, I know this is silly. But it was easy.-->
+				AfPA is led by David Charles, M.D., who is the Chief Medical Officer of the Vanderbilt University Clinical Neurosciences Institute. Dr. Charles is a national leader in Movement Disorders research. He took leave from his practice in 1998 and spent a year on the staff of U.S. Senator Bill Frist, where he served as a health policy advisor. Following this experience in Washington, Dr. Charles conducted Parkinson’s disease research in France as a Fulbright Senior Scholar.</p>
+			<br><hr style="border: 1px solid #B0D8BC;"><br>
+			<img style="max-height: 200px; float: right; padding-left: 10px" src="<?php echo get_stylesheet_directory_uri(); ?>/img/profiles/Howard-Hoffberg.png">
+			<p style="text-align: right;">
+				<strong>Howard Hoffberg, M.D.<br>
+				Owings Mills, Maryland</strong></p><br><br><br><br><br>
+			<p style="text-align: justify;">
+				Dr. Hoffberg is associate medical director of Rehabilitation and Pain Managements Associates, a freestanding private practice specializing in multidisciplinary treatment (including Physical Therapy and Psychosocial Counseling) for pain located in the surburban Baltimore region. He was a former president of the Maryland Society of Physical Medicine and Rehabilitation, and is currently the Highmark Medicare Part B Carrier Advisor Representative in Maryland. He has written book chapters and journal articles in the subjects of Rehabilitation and Pain, and has been involved in training residents and physician assistants.</p>
+			<br><hr style="border: 1px solid #B0D8BC;"><br>
+			<img style="max-height: 200px; float: left; padding-right: 10px" src="<?php echo get_stylesheet_directory_uri(); ?>/img/profiles/Jack-Shim.png">
+			<p style="min-height: 200px; text-align: justify;">
+				<strong>Jack Shim, M.D.<br>
+				Oceanside, California</strong><br><br>
+					Dr. Schim is Voluntary Assistant Clinical Professor in the Neuroscience Department at University of California San Diego, and is on staff at the VA Medical Center in La Jolla. He has special interests in headache management, stroke prevention, acute stroke intervention, and neurologic rehabilitation.<br><br>
+
+					Dr. Schim earned his medical degree from University of California San Diego (UCSD) Medical School, served as a medical intern at Cedars Sinai Medical Center, Los Angeles, and completed his neurology residency at UCSD, with special emphasis on electromyography and neuromuscular disease. He is Past President of the Association of California Neurologists and an active member of the American Academy of Neurology, American Stroke Association, and American Headache Society. Dr. Schim is Chair of the Division of Neuroscience at Scripps Hospital, Encinitas.<br><br>
+
+					Dr. Schim is Board certified in Neurology, and has certification in Headache Medicine by the United Council of Neurologic Subspecialties. He has published articles in Experimental Neurology, Current Medical Research and Opinion, Pain Practice, Headache and Neurology.</p>
+			<br><hr style="border: 1px solid #B0D8BC;"><br>
+			<img style="max-height: 200px; float: right; padding-left: 10px" src="<?php echo get_stylesheet_directory_uri(); ?>/img/profiles/Robert-Yapundich.png">
+			<p style="text-align: right;">
+				<strong>Robert Yapundich, M.D.<br>
+				Hickory, North Carolina</strong></p><br>
+			<p style="text-align: justify;">
+				Dr. Robert Yapundich is a neurologist in private practice with Neurology Associates in Hickory, N.C.<br><br>
+				Dr. Yapundich completed a neurology residency at the University of Alabama at Birmingham, followed by a fellowship in electromyography/neuromuscular diseases. He is a diplomat of The American Board of Psychiatry and Neurology with subspecialty certifications in Clinical Neurophysiology and Sleep Medicine, along with certification by the American Board of Electrodiagnostic Medicine.<br><br>
+				Dr. Yapundich maintains an active outpatient practice that involves all aspects of Neurology, with a special interest in Electrodiagnostic Medicine. He has served as president of the North Carolina Neurological Society, and participates in various other local and statewide committees. Currently he is a board member of the North Carolina Medical Society and state liason for the American Association of Neuromuscular and Electrodiagnostic Medicine. Healthcare advocacy at the state and national levels has also been an area of significant interest. He has participated in clinical trials, many as primary investigator, and has served as co-author on a variety of past publications.</p>
+			<br><hr style="border: 1px solid #B0D8BC;"><br>
+			<img style="max-height: 200px; float: left; padding-right: 10px" src="<?php echo get_stylesheet_directory_uri(); ?>/img/profiles/Robin-K.-Dore.png">
+			<p style="min-height: 200px; text-align: justify;">
+				<strong>Robin K. Dore, M.D.<br>
+				Tustin, California</strong><br><br>
+				Robin K. Dore, M.D., is a Board certified rheumatologist in private practice in Tustin, Calif. and a Clinical Professor of Medicine at UCLA.<br><br> Dr. Dore is Chair of the Orange County Branch of the Arthritis Foundation, Pacific Region.<br><br> She is a past president of the Southern California Rheumatology Society and a past member of the board of directors of the American College of Rheumatology.<br><br> A clinical investigator, Dr. Dore participates in clinical research studies of medications for treatment of gout, osteoarthritis, osteoporosis, rheumatoid arthritis, and other metabolic bone diseases.</p>
+		</section>
+	</center>
 </main>
 <?php get_footer(); ?>
