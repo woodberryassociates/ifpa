d5bf79b3ac3d11d8d6726bb660447a323cc9ffa2
Avi Nerenberg <devavmeister@gmail.com>
2017-12-12 00:33:00 -0500

    added 'about us' page template

---
+++ wp-content/themes/ifpa_custom/about-page.php
@@ -0,0 +1,14 @@
+<?php /*Template Name: About Us*/ ?>
+
+<?php get_header(); ?>
+
+<main role="main">
+	<section>
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/profiles/David-Charles.png">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/profiles/Howard-Hoffberg.png">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/profiles/Jack-Shim.png">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/profiles/Robert-Yapundich.png">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/profiles/Robin-K.-Dore.png">
+	</section>
+</main>
+<?php get_footer(); ?>
