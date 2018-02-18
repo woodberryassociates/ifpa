ae19357b9e4be4d411c7685fb3e2988e3c1c61b2
Avi Nerenberg <devavmeister@gmail.com>
2018-02-17 22:25:56 -0500

    add custom templates for policy papers, report cards, and studies

---
+++ wp-content/themes/ifpa_custom/s-page.php
@@ -0,0 +1,24 @@
+<?php /*Template Name: Studies*/ ?>
+
+<?php get_header(); ?>
+
+<main role="main">
+	<section class="single">
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+			<?php the_content() ?>
+		<?php endwhile; ?>
+	<?php endif; ?>
+	</section>
+
+	<section>
+		<p style="font-size: 35px; color: #2AAD69; padding: 10px 0px 10px 2vw">Posts</p> 
+		<?php
+			$catObj = get_category_by_slug('studies'); 
+			$catId = $catObj->term_id;
+			//$catquery = new WP_Query('cat='.$catId);
+			query_posts('cat='.$catId);
+			get_template_part('loop');
+		?>
+	</section>
+</main>
+<?php get_footer(); ?>
