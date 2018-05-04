c7977d21bf61b2c97398430f0cbfa31134ecf2de
Avi Nerenberg <devavmeister@gmail.com>
2018-05-03 21:09:43 -0400

    updating front page layout

--- wp-content/themes/ifpa_custom/front-page.php
+++ wp-content/themes/ifpa_custom/front-page.php
@@ -12,21 +12,23 @@
 		<!-- /article -->
 	</section>
 
-	<section style="padding-top: 150px; background-color: #F6F6F6">
-		<h1>Recent Posts</h1>
-		<?php
-			query_posts('posts_per_page=4');
-			get_template_part('loop');
-			/*$recent_posts = new WP_Query('posts_per_page=3'); /*this number determines how many recent posts display on the home page*/
-			/*while($recent_posts->have_posts()) : $recent_posts->the_post(); ?>
-				<section class="home-block-article">
-					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-				</section>
-		<?php
-			endwhile;*/
-			wp_reset_postdata();	
-		?>
-	</section>
+	<div id="front-page-container">
+		<section id="whats-new">
+			<h1>Recent Posts</h1>
+			<?php
+				query_posts('posts_per_page=4');
+				get_template_part('loop');
+				/*$recent_posts = new WP_Query('posts_per_page=3'); /*this number determines how many recent posts display on the home page*/
+				/*while($recent_posts->have_posts()) : $recent_posts->the_post(); ?>
+					<section class="home-block-article">
+						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+						<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+					</section>
+			<?php
+				endwhile;*/
+				wp_reset_postdata();	
+			?>
+		</section>
+	</div>
 </main>
 <?php get_footer(); ?>
