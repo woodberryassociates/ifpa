34874263466062397751c8f8bc3a09b88deb8f9a
Avi Nerenberg <devavmeister@gmail.com>
2018-05-04 18:45:28 -0400

    modding splash page

--- wp-content/themes/ifpa_custom/loop.php
+++ wp-content/themes/ifpa_custom/loop.php
@@ -21,7 +21,7 @@
 			<!-- /post title -->
 			<!-- post date -->
 			<h2 style="display: flex; padding-left: 10px; text-transform: uppercase;">
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_time(get_option('date_format')); ?></a>
+				<?php the_time(get_option('date_format')); ?>
 			</h2>
 			<!-- /post date -->
 
