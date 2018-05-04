60271a39f3c1358b01701e245599a9a8f55c6b65
Avi Nerenberg <devavmeister@gmail.com>
2018-05-04 18:39:34 -0400

    modding splash page

--- wp-content/themes/ifpa_custom/loop.php
+++ wp-content/themes/ifpa_custom/loop.php
@@ -1,56 +1,63 @@
+<?php $articleCount = 0; ?>
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 	<!--if the post title contains no spaces AND isn't titled "infographic" (i.e. not content), skip it (hack to display content media)-->
 	<?php if (!strpos(the_title('','',false),' ') && strpos(the_title('','',false),"Infographic")) {
 		continue;
 	} ?>
 
-		<!-- article -->
-		<article style="/*padding: 0 15vw 0 15vw;*/ display: block; min-height: 220px" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<!-- article -->
+	<article style="/*padding: 0 15vw 0 15vw;*/ display: block; min-height: 220px" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<?php if (has_post_thumbnail()): // Check if thumbnail exists ?>
-				<!-- post thumbnail -->
-				<a class="loop-thumbnail" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(array(300,200)); // Declare pixel size you need inside the array ?>
-				</a>
-				<!-- /post thumbnail -->
-				<!-- post title -->
-				<h1 style="display: flex; padding-left: 10px;">
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-				</h1>
-				<!-- /post title -->
-				<!-- post date -->
-				<h2 style="display: flex; padding-left: 10px;">
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_time(get_option('date_format')); ?></a>
-				</h2>
-				<!-- /post date -->
+		<?php if (has_post_thumbnail()): // Check if thumbnail exists ?>
+			<!-- post thumbnail -->
+			<a class="loop-thumbnail" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+				<?php the_post_thumbnail(array(300,200)); // Declare pixel size you need inside the array ?>
+			</a>
+			<!-- /post thumbnail -->
+			<!-- post title -->
+			<h1 style="display: flex; padding-left: 10px;">
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+			</h1>
+			<!-- /post title -->
+			<!-- post date -->
+			<h2 style="display: flex; padding-left: 10px;">
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_time(get_option('date_format')); ?></a>
+			</h2>
+			<!-- /post date -->
 
-				<?php echo "<h2 style='display: flex; padding: 5px 10px;'"; // dumb hack to correctly style the excerpt ?>
-				<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
-				<?php echo "</h2><a style='display: flex; padding: 0px 10px;'"; ?>
-				<?php edit_post_link(); ?>
-				<?php echo "</a>"; ?>
+			<?php echo "<h2 style='display: flex; padding: 5px 10px;'"; // dumb hack to correctly style the excerpt ?>
+			<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+			<?php echo "</h2><a style='display: flex; padding: 0px 10px;'"; ?>
+			<?php edit_post_link(); ?>
+			<?php echo "</a>"; ?>
 
-			<?php else: //if no thumbnail ?>
-				<!-- post title -->
-				<h1 style="padding: 10px 10px 0px 310px;">
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-				</h1>
-				<!-- /post title -->
-				<!-- post date -->
-				<h2 style="display: flex; padding: 0px 10px 10px 310px;">
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_time(get_option('date_format')); ?></a>
-				</h2>
-				<!-- /post date -->
+		<?php else: //if no thumbnail ?>
+			<!-- post title -->
+			<h1 style="padding: 10px 10px 0px 310px;">
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+			</h1>
+			<!-- /post title -->
+			<!-- post date -->
+			<h2 style="display: flex; padding: 0px 10px 10px 310px;">
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_time(get_option('date_format')); ?></a>
+			</h2>
+			<!-- /post date -->
 
-				<?php echo "<h2 style='display: flex; padding: 5px 10px 5px 310px;'"; // dumb hack to correctly style the excerpt ?>
-				<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
-				<?php echo "</h2><a style='display: flex; padding: 0px 10px 0px 310px;'"; ?>
-				<?php edit_post_link(); ?>
-				<?php echo "</a>"; ?>
-			<?php endif; ?>
-		</article>
-		<!-- /article -->
+			<?php echo "<h2 style='display: flex; padding: 5px 10px 5px 310px;'"; // dumb hack to correctly style the excerpt ?>
+			<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+			<?php echo "</h2><a style='display: flex; padding: 0px 10px 0px 310px;'"; ?>
+			<?php edit_post_link(); ?>
+			<?php echo "</a>"; ?>
+		<?php endif; ?>
+	</article>
+	<!-- /article -->
+
+	<!--adding breaks between articles-->
+	<?php $articleCount++; ?>
+	<?php if($articleCount <= 3): ?>
 		<div style="margin-bottom: 20px; height: 1px; background-color: #707070"></div>
+	<?php endif; ?>
+
 <?php endwhile; ?>
 <?php else: ?>
 	<!-- article -->
