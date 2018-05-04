485054d7f02d57a3aff7ee94abb646345643ee13
Avi Nerenberg <devavmeister@gmail.com>
2018-05-04 16:15:53 -0400

    modding splash page

--- wp-content/themes/ifpa_custom/front-page.php
+++ wp-content/themes/ifpa_custom/front-page.php
@@ -12,30 +12,32 @@
 		<!-- /article -->
 	</section>
 
-	<div id="front-page-container">
-		<section id="whats-new">
-			<h1>What's New</h1>
-			<?php
-				query_posts('posts_per_page=4');
-				get_template_part('loop');
-				/*$recent_posts = new WP_Query('posts_per_page=3'); /*this number determines how many recent posts display on the home page*/
-				/*while($recent_posts->have_posts()) : $recent_posts->the_post(); ?>
-					<section class="home-block-article">
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-						<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-					</section>
-			<?php
-				endwhile;*/
-				wp_reset_postdata();	
-			?>
-		</section>
-		<section id="latest">
-			<h1>Latest</h1>
-		</section>
+	<div id="front-page-main">
+		<div id="front-page-container">
+			<section id="whats-new">
+				<h1>What's New</h1>
+				<?php
+					query_posts('posts_per_page=4');
+					get_template_part('loop');
+					/*$recent_posts = new WP_Query('posts_per_page=3'); /*this number determines how many recent posts display on the home page*/
+					/*while($recent_posts->have_posts()) : $recent_posts->the_post(); ?>
+						<section class="home-block-article">
+							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+							<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+						</section>
+				<?php
+					endwhile;*/
+					wp_reset_postdata();	
+				?>
+			</section>
+			<section id="latest">
+				<h1>Latest</h1>
+			</section>
 
-		<section id="recent-blog-posts">
-			
-		</section>
+			<section id="recent-blog-posts">
+				
+			</section>
+		</div>
 	</div>
 </main>
 <?php get_footer(); ?>
