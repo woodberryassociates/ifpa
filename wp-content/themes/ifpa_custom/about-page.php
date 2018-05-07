d36b1c15734ced51e4dcbe3881bb529e5310f20e
Avi Nerenberg <devavmeister@gmail.com>
2018-05-07 16:49:21 -0400

    updating about page

--- wp-content/themes/ifpa_custom/about-page.php
+++ wp-content/themes/ifpa_custom/about-page.php
@@ -10,9 +10,13 @@
 
 	<section id="split-page-main">
 		<section id="split-page-container" class="gray" style="margin: 30px auto; padding: 0 20px;">
+			<!-- article -->
 			<?php while ( have_posts()) : the_post(); ?> 
 				<?php the_content(); ?>
 			<?php endwhile; ?>
+			<!-- /article -->
+
+			<h1 class="page-header" style="text-transform: none; position: relative; right:  42%;">IfPA LEADERSHIP</h1>
 			<section class="bio">
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/profiles/David-Charles.png"><div></div>
 				<h2 class="bio-name">David Charles, M.D.</h2>
