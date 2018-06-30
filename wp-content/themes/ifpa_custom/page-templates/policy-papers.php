18fbcc0bd3c2f46c6989423567400e932e13f280
Avi <avi@allianceforpatientaccess.org>
2018-06-29 20:57:56 -0400

    fixing dating on backpage years

--- wp-content/themes/ifpa_custom/page-templates/policy-papers.php
+++ wp-content/themes/ifpa_custom/page-templates/policy-papers.php
@@ -62,11 +62,11 @@
 
 							//print years
 							foreach ($years as $year) :
-								if ($year != $year) :
+								if ($year != date('Y')) :
 						?>
 							<h1 class="clickable year" itemYear="<?php echo $year ?>" itemType="policy-briefs"><?php echo $year ?></h1>
 
-						<?php elseif ($year == $year) : ?>
+						<?php elseif ($year == date('Y')) : ?>
 
 							<h1 style="color: #282f5d" class="clickable year" itemYear="<?php echo $year ?>" itemType="policy-briefs"><?php echo $year ?></h1>
 						
