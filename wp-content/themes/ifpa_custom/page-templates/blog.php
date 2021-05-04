0678829a0f846dca158b435e5cd6f28c8b399f76
Avi Nerenberg <devavmeister@gmail.com>
2021-05-03 23:40:13 -0400

    feat(ads): ads header & footer ad groups to blog page, Advanced Ads config is still WIP

--- wp-content/themes/ifpa_custom/page-templates/blog.php
+++ wp-content/themes/ifpa_custom/page-templates/blog.php
@@ -3,41 +3,47 @@
 <?php get_header(); ?>
 
 <main role="main">
+	<!-- header ads -->
+	<?php if (function_exists('the_ad_group')) the_ad_group(50); ?>
+	<!-- /header ads -->
 
 	<!-- header img -->
-	<?php while ( have_posts()) : the_post();
+	<?php while (have_posts()) : the_post();
 		the_post_thumbnail('full');
 	endwhile; ?>
 	<!-- /header img -->
 
-	<!-- section -->
+	<!-- content -->
 	<section class="single-full">
-		
+
 		<!-- blog page content -->
-		<?php while ( have_posts()) : the_post();
+		<?php while (have_posts()) : the_post();
 			the_content();
 		endwhile; ?>
 		<!-- /blog page content -->
 
 		<?php
-			$catObj = get_category_by_slug('blog'); 
-			$catId = $catObj->term_id;
-			//$catquery = new WP_Query('cat='.$catId);
-			query_posts('cat='.$catId.'&&posts_per_page=1');
+		$catObj = get_category_by_slug('blog');
+		$catId = $catObj->term_id;
+		//$catquery = new WP_Query('cat='.$catId);
+		query_posts('cat=' . $catId . '&&posts_per_page=1');
 		?>
 
 		<h2 style="margin: 2em 0 1em; text-transform: uppercase; display: inline;">Latest Post</h2>
 
 		<!-- search widget -->
-		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Blog Search") ) : ?>
-		<?php endif;?>
+		<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar("Blog Search")) : ?>
+		<?php endif; ?>
 		<!-- /search widget -->
 
-		<?php get_template_part('loop', 'blog') // loop-blog.php ?>
-
+		<?php get_template_part('loop', 'blog') // loop-blog.php
+		?>
 	</section>
-	<!-- /section -->
+	<!-- /content -->
 
+	<!-- footer ads -->
+	<?php if (function_exists('the_ad_group')) the_ad_group(51); ?>
+	<!-- /footer ads -->
 </main>
 
 <?php get_footer(); ?>
