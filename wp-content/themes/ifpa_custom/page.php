5da619cd5420eb9d2f9dee95d84acfa0962f120b
Avi Nerenberg <devavmeister@gmail.com>
2022-04-21 01:33:41 -0400

    styles(templates): adds basic content class to default page template (`single-full`)
    - implements `recent_posts` template part in default template to remove dup logic
    - some minor reformatting w/ current linter prefs

--- wp-content/themes/ifpa_custom/page.php
+++ wp-content/themes/ifpa_custom/page.php
@@ -1,49 +1,31 @@
 <?php get_header(); ?>
+
 <main role="main">
-	<!-- section -->
-	<section>
+	<!-- content -->
+	<section class="single-full">
 		<h1 class="page-header"><?php the_title(); ?></h1>
-		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+		<?php if (have_posts()) :
+			while (have_posts()) : the_post(); ?>
+				<!-- article -->
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php the_content(); ?>
+					<!--?php comments_template( '', true ); // Remove if you don't want comments ?-->
+					<br class="clear">
+					<?php edit_post_link(); ?>
+				</article>
+				<!-- /article -->
+			<?php endwhile;
+		else : ?>
 			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<?php the_content(); ?>
-				<!--?php comments_template( '', true ); // Remove if you don't want comments ?-->
-				<br class="clear">
-				<?php edit_post_link(); ?>
+			<article>
+				<h2><?php _e('Sorry, nothing to display.', 'html5blank'); ?></h2>
 			</article>
 			<!-- /article -->
-		<?php endwhile; ?>
-	<?php else: ?>
-		<!-- article -->
-		<article>
-			<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-		</article>
-		<!-- /article -->
-	<?php endif; ?>
-</section>
-<!-- /section -->
+		<?php endif; ?>
+	</section>
+	<!-- /content -->
 </main>
-<!--recent blog posts-->
-			<section id="recent-blog-posts">
-				<h1 style="padding-left: 6vw; font-weight: bold; text-transform: uppercase; color: white;">Recent Blog Posts</h1>
-			</section>
 
-			<section class="home-block">
-				<?php
-					$catObj = get_category_by_slug('blog'); 
-					$catId = $catObj->term_id;
-					$recent_posts = new WP_Query('cat='.$catId.'&&posts_per_page=3');
-					while($recent_posts->have_posts()) : $recent_posts->the_post();
-				?>
-						<section class="home-block-article">
-							<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-							<a class="home-block-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-							<a class="home-block-date" href="<?php the_permalink(); ?>" style="font-size: .7em"><?php the_time(get_option('date_format')); ?></a>
-						</section>
-				<?php
-					endwhile;
-					wp_reset_postdata();	
-				?>
-			</section>
-			<!--/recent blog posts-->
+<?php get_template_part('recent-posts'); ?>
+
 <?php get_footer(); ?>
