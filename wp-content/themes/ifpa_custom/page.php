0e3b33b733de80be1bffe648541ea1c697321d4c
Avi Nerenberg <devavmeister@gmail.com>
2022-04-21 02:00:01 -0400

    feat(templates): dup's portions of `loop-single.php` for the default page template, to Erin's requests

--- wp-content/themes/ifpa_custom/page.php
+++ wp-content/themes/ifpa_custom/page.php
@@ -3,22 +3,40 @@
 <main role="main">
 	<!-- content -->
 	<section class="single-full">
-		<!-- title -->
-		<h1 class="page-header"><?php the_title(); ?></h1>
-
 		<!-- article -->
-		<?php if (have_posts()) :
-			while (have_posts()) : the_post(); ?>
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+				<?php
+				ob_start();
+				the_content();
+				$content = ob_get_clean();
+				?>
+
+				<!-- article -->
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<?php the_content(); ?>
-					<br class="clear">
-					<?php edit_post_link(); ?>
+					<article id="content">
+
+						<!-- page title -->
+						<h1 class="page-header" style="padding: 0; line-height: 1em;"><?php the_title(); ?></h1>
+						<!-- /page title -->
+
+						<!-- page content -->
+						<?php the_content(); ?>
+						<!-- /page content -->
+
+						<p style="text-align: right;"><?php edit_post_link(); ?></p>
+					</article>
 				</article>
-			<?php endwhile;
-		else : ?>
-			<article>
-				<h2><?php _e('Sorry, nothing to display.', 'html5blank'); ?></h2>
+				<!-- /article -->
+			<?php endwhile; ?>
+		<?php else : ?>
+
+			<!-- article -->
+			<article id="content">
+				<h1><?php _e('Sorry, nothing to display.', 'html5blank'); ?></h1>
 			</article>
+			<!-- /article -->
+
 		<?php endif; ?>
 		<!-- /article -->
 	</section>
