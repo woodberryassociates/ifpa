56ae6a19ea4f6a97935add82cbba0a71543d98b6
Avi Nerenberg <devavmeister@gmail.com>
2022-04-25 22:44:04 -0500

    feat(speakers): dup's "About Us" page for "Speakers" page (w/ `custom_fields` content)

---
+++ wp-content/themes/ifpa_custom/page-templates/speakers.php
@@ -0,0 +1,44 @@
+<?php /*Template Name: Speakers*/ ?>
+<!-- NOTE - dup of "About Us" -->
+
+<?php get_header(); ?>
+
+<main role="main">
+
+	<!-- header img -->
+	<?php while (have_posts()) : the_post();
+		the_post_thumbnail('full');
+	endwhile; ?>
+	<!-- /header img -->
+
+	<!-- article -->
+	<?php while (have_posts()) : the_post(); ?>
+		<?php the_content(); ?>
+
+		<section id="split-page-main">
+			<section id="split-page-container" class="gray" style="margin: 30px auto; padding: 0 20px;">
+
+				<h1 class="page-header" style="text-transform: uppercase; position: relative; right:  42%;">Event Speakers</h1>
+
+				<?php
+				$custom_fields = get_post_custom();
+				$board_members = $custom_fields['speakers'];
+				$last_member = end($board_members); // used to drop the divider for the last bio
+
+				foreach ($board_members as $key => $value) {
+					echo  $value;
+					if ($value != $last_member) // drop the divider for the last bio
+						echo '<div class="divider" style="width: 100%; height: 1px;"></div>';
+				}
+				?>
+
+			</section>
+		</section>
+	<?php endwhile; ?>
+	<!-- /article -->
+
+</main>
+
+<?php get_template_part('recent-posts'); ?>
+
+<?php get_footer(); ?>
