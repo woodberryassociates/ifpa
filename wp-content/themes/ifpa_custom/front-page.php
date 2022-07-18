9be7ec1e74c3480ecd6e5c5adc47afdf896afe34
Avi Nerenberg <devavmeister@gmail.com>
2022-07-17 21:49:58 -0400

    feat(home-page): new format (blog being moved to [new site](https://healthpolicytoday.org/))
    - minor fixes & improvements to the home page & loop

--- wp-content/themes/ifpa_custom/front-page.php
+++ wp-content/themes/ifpa_custom/front-page.php
@@ -10,95 +10,69 @@
 			<?php the_content(); ?>
 		</article>
 		<!-- /article -->
-		
+
 	</section>
 
 	<div id="split-page-main" class="gray front">
 		<div style="height: 150px; position: relative; bottom: 150px;"></div>
 
 		<div id="split-page-container" style="padding-bottom: 60px;">
-			
 			<section id="split-page-left">
-
-				<h1 class="page-header" style="padding: 0;">What's New on the Blog</h1>
+				<!-- policy papers -->
+				<h1 class="page-header" style="padding: 0;">Latest Policy Papers</h1>
 
 				<?php
-					$catObj = get_category_by_slug('blog'); 
-					$catId = $catObj->term_id;
+				$catObj = get_category_by_slug('policy-papers');
+				$catId = $catObj->term_id;
 
-					$args = array(
-						'cat' => $catId,
-						'posts_per_page' => 5,
-						'frontPage' => true
-					);
+				$args = array(
+					'cat' => $catId,
+					'posts_per_page' => 2,
+					'frontPage' => true
+				);
 
-					$wp_query = new WP_Query($args);
-					get_template_part('loop');
-					wp_reset_postdata();
+				$wp_query = new WP_Query($args);
+				get_template_part('loop');
+				wp_reset_postdata();
 				?>
+				<!-- /policy papers -->
 
+				<!-- summits -->
+				<h1 class="page-header" style="padding: 0;">Latest Summits</h1>
+
+				<?php
+				$catObj = get_category_by_slug('summits');
+				$catId = $catObj->term_id;
+
+				$args = array(
+					'cat' => $catId,
+					'posts_per_page' => 2,
+					'frontPage' => true
+				);
+
+				$wp_query = new WP_Query($args);
+				get_template_part('loop');
+				wp_reset_postdata();
+				?>
+				<!-- /summits -->
 			</section>
 
 			<section class="front-page" id="split-page-right">
-				<h1 class="page-header-inverted">Latest</h1>
-
-				<!-- policy papers -->
-				<h2 class="page-header" style="padding: 0 12% 0 12%; line-height: 75px; position: relative; top: 15px;">Policy Papers</h2>
-				
-				<?php
-					$count = 0; //used to insert dividers b/w articles (TODO)
-					$catObj = get_category_by_slug('policy-papers'); 
-					$catId = $catObj->term_id;
-
-					$recent_posts = new WP_Query('cat='.$catId.'&&posts_per_page=3');
-					while($recent_posts->have_posts()) :
-						$recent_posts->the_post();
-				?>			
-						<section class="split-page-no-thumbnail-article" style="padding: 0 12%; margin: 10px 0 10px 0">
-							<a class="no-thumbnail-article-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-							<p class="no-thumbnail-article-date" href="<?php the_permalink(); ?>" style="font-size: .7em; text-transform: uppercase;"><?php the_time('F Y'); // locations hardcoded as 'F Y' only show month & year ?></p>
-						</section>
-				<?php
-					endwhile;
-					wp_reset_postdata();
-				?>
-				<!-- /policy papers -->
+				<h1 class="page-header-inverted">Other Resources</h1>
 
 				<!-- twitter widget -->
 				<h2 class="page-header" style="padding: 0 12% 0 12%; line-height: 75px;">Twitter</h2>
 
-				<a class="twitter-timeline" href="https://twitter.com/patientaccess"  data-width="260" data-tweet-limit="1" data-chrome="nofooter noborders noheader transparent">Tweets by Alliance for Patient Access</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
+				<a class="twitter-timeline" href="https://twitter.com/patientaccess" data-width="260" data-tweet-limit="1" data-chrome="nofooter noborders noheader transparent">Tweets by Alliance for Patient Access</a>
+				<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 				<!-- /twitter widget -->
 
-				<!-- summits -->
-				<h2 class="page-header" style="padding: 0 12% 0 12%; line-height: 75px;">Policy Summits</h2>
-
-				<?php
-					$count = 0; //used to insert dividers b/w articles (TODO)
-					$catObj = get_category_by_slug('summits'); 
-					$catId = $catObj->term_id;
-
-					$recent_posts = new WP_Query('cat='.$catId.'&&posts_per_page=2');
-					while($recent_posts->have_posts()) :
-						$recent_posts->the_post();
-				?>			
-						<section class="split-page-no-thumbnail-article" style="padding: 0 12%; margin: 10px 0 10px 0">
-							<a class="no-thumbnail-article-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-							<p class="no-thumbnail-article-date" href="<?php the_permalink(); ?>" style="font-size: .7em; text-transform: uppercase;"><?php the_time('F Y'); ?></p>
-						</section>
-				<?php
-					endwhile;
-					wp_reset_postdata();
-				?>
-				<!-- /summits -->
-
 				<!-- BAA Blog -->
-				<a href="<?php echo get_site_url(); echo '/tag/by-all-accounts/'; ?>"><img src="<?php echo get_template_directory_uri(); echo '/img/BAA-logo.png'; ?>"></a>
+				<a href="<?php echo get_site_url() . '/tag/by-all-accounts/'; ?>"><img src="<?php echo get_template_directory_uri() . '/img/BAA-logo.png'; ?>" alt="By All Accounts Blog"></a>
 				<!-- /BAA Blog -->
-
 			</section>
 		</div>
 	</div>
 </main>
-	
+
 <?php get_footer(); ?>
--- wp-content/themes/ifpa_custom/loop.php
+++ wp-content/themes/ifpa_custom/loop.php
@@ -1,57 +1,59 @@
 <?php $articleCount = 0; ?>
 
-<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-	<!-- article -->
-	<article style="display: block; min-height: 220px" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<!-- article -->
+		<article style="display: block; min-height: 220px" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<!-- post thumbnail -->
-		<a class="loop-thumbnail" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-			<?php the_post_thumbnail(); // Declare pixel size you need inside the array ?>
-		</a>
-		<!-- /post thumbnail -->
+			<!-- post thumbnail -->
+			<a class="loop-thumbnail" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+				<?php the_post_thumbnail(); // Declare pixel size you need inside the array
+				?>
+			</a>
+			<!-- /post thumbnail -->
 
-		<!-- post title -->
-		<h1 style="display: flex; padding-left: 10px;">
-			<a class="thumbnail-article-title" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-		</h1>
-		<!-- /post title -->
+			<!-- post title -->
+			<h1 style="display: flex; padding-left: 10px;">
+				<a class="thumbnail-article-title" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+			</h1>
+			<!-- /post title -->
 
-		<!-- post date -->
-		<h1 class="thumbnail-article-date" style="display: flex; padding: 5px 0 0 10px; font-size: 1.2em; font-weight: 500">
-			<?php the_time(get_option('date_format')); ?>
-		</h1>
-		<!-- /post date -->
+			<!-- post date -->
+			<h1 class="thumbnail-article-date" style="display: flex; padding: 5px 0 0 10px; font-size: 1.2em; font-weight: 500">
+				<?php the_time(get_option('date_format')); ?>
+			</h1>
+			<!-- /post date -->
 
-		<!-- excerpt, read more, edit post links -->
-		<?php echo "<h2 class='thumbnail-article-text'"; // hack to correctly style the excerpt ?>
-		<?php html5wp_excerpt('html5wp_index'); ?>
-		<?php echo "</h2><a style='display: flex; padding: 0px 10px;'"; ?>
-		<?php edit_post_link(); ?>
-		<?php echo "</a>"; ?>
-		<!-- /excerpt, read more, edit post links -->
-		
-	</article>
-	<!-- /article -->
+			<!-- excerpt, read more, edit post links -->
+			<?php if (!$frontPage) {
+				echo "<h2 class='thumbnail-article-text'"; // hack to correctly style the excerpt
+				html5wp_excerpt('html5wp_index');
+				echo "</h2><a style='display: flex; padding: 0px 10px;'";
+				edit_post_link();
+				echo "</a>";
+			} ?>
+			<!-- /excerpt, read more, edit post links -->
 
-	<!-- breaks between articles -->
-	<?php $articleCount++; ?>
-	<?php if($frontPage && $articleCount <= 4): // front page only has 5 articles, so hardcoding this ?>
-		<div class="divider" style="margin-bottom: 20px; height: 1px; width: 100%;"></div>
+		</article>
+		<!-- /article -->
 
-	<?php elseif($frontPage == false && $articleCount % get_option('posts_per_page') != 0 && $articleCount < $wp_query->found_posts): ?> 
-		<div class="divider" style="margin-bottom: 20px; height: 1px; width: 100%;"></div>
+		<!-- breaks between articles -->
+		<?php $articleCount++; ?>
+		<?php if ($frontPage && $articleCount <= 1) : // front page only has 5 articles, so hardcoding this
+		?>
+			<div class="divider" style="margin-bottom: 20px; height: 1px; width: 100%;"></div>
+		<?php elseif (!$frontPage && $articleCount % get_option('posts_per_page') != 0 && $articleCount < $wp_query->found_posts) : ?>
+			<div class="divider" style="margin-bottom: 20px; height: 1px; width: 100%;"></div>
+		<?php endif; ?>
+		<!-- /breaks between articles -->
 
-	<?php endif; ?>
-	<!-- /breaks between articles -->
-
-<?php endwhile; ?>
-<?php else: ?>
+	<?php endwhile; ?>
+<?php else : ?>
 
 	<!-- article -->
 	<article>
-		<h1 style="text-align: center; color: #2AAD69; padding-bottom: 20px;"><?php _e( 'Sorry, nothing to display.		:(', 'html5blank' ); ?></h1>
+		<h1 style="text-align: center; color: #2AAD69; padding-bottom: 20px;"><?php _e('Sorry, nothing to display.		:(', 'html5blank'); ?></h1>
 	</article>
 	<!-- /article -->
-	
+
 <?php endif; ?>
